6e74f70af5857ecb88e1eb756f028f3cc12d2721
rkashyap1 <kashyap.ravi15@yahoo.in>
2018-05-07 23:29:34 +0000

    updated_network

--- network.tf
+++ network.tf
@@ -150,5 +150,28 @@ resource "aws_nat_gateway" "gw-b" {
 	}
 
 #------------------------------------------------------------#
+	             #SECURITY GROUP#                         
+#------------------------------------------------------------#
+resource "aws_security_group" "private-1a" {
+        vpc_id = "${aws_vpc.default.id}"
+        description = "SG for private instances"
+        name = "PRIVATE SG"
+        tags    {
+                name = "${var.SG_name}"
+                }
+        ingress {
+                from_port = 0
+                to_port = 65535
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+                }
+        egress {
+                from_port = 0
+                to_port = 65535
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+                }
+        }
+#------------------------------------------------------------#
 		#FINISH:-  RAVI KASHYAP#
 #------------------------------------------------------------#
